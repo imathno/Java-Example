decf666e633d037c6d94bf0bed0f6a3ddfc2cc19
Issa Mathno <imathno@gmail.com>
2020-03-19 21:52:00 -0700

    Add more algo work

--- src/main/java/team/whatever/algorithm/Group.java
+++ src/main/java/team/whatever/algorithm/Group.java
@@ -1,21 +1,42 @@
 package team.whatever.algorithm;
 
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
 import team.whatever.community.Person;
 
+import java.util.List;
 import java.util.Map;
 import org.jgrapht.Graph;
 
 public class Group {
 
-    private Person[] community;
-    private Map<Person, Boolean> visitedHomes;
+    private final int GROUP_SIZE = 4;
 
-    public Group(Person[] community) {
-        this.community = community;
+    private final List<Person> COMMUNITY;
+    private int numOfGroups;
+    private int iterations;
+    private Map<Person, Boolean> hosted;
+
+    public Group(List<Person> community) {
+        this.COMMUNITY = community;
+        int actualSize = community.size();
+
+        for (int i = 0; i < community.size(); i++) {
+            if (community.get(i).hasSpouse())
+                actualSize++;
+        }
+
+        this.numOfGroups = actualSize / GROUP_SIZE;
+        this.iterations = community.size() / this.numOfGroups;
     }
 
     public Graph<Person, DefaultEdge> generateGraph() {
+        Graph<Graph<List<List<Person>>, DefaultUndirectedGraph>, DefaultUndirectedGraph> finalGraph;
+        for (int i = 0; i < this.iterations; i++) {
+            for (int j = 0; j < this.numOfGroups; j++) {
+                
+            }
+        }
         return null;
     }
 }
